aa71b68ae256f9b0469d8b80b7db8a16d4e4d59f
Daniel Liebert <daniel.liebert@gmail.com>
2022-08-05 16:19:58 -0300

    Create solutions

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,66 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    for i in 0..self.length-1
+      yield self[i], i
+    end
+    self
+  end
+
+  def my_select
+    selected = []
+    for i in 0..self.length-1
+      selected << self[i] if yield self[i]
+    end
+    selected
+  end
+
+  def my_all?
+    for i in 0..self.length-1
+      return false unless yield self[i]
+    end
+    true
+  end
+
+  def my_any?
+    for i in 0..self.length-1
+      return true if yield self[i]
+    end
+    false
+  end
+
+  def my_none?
+    for i in 0..self.length-1
+      return false if yield self[i]
+    end
+    true
+  end
+
+  def my_count
+    counter = 0
+    if block_given?
+      for i in 0..self.length-1
+        counter += 1 if yield self[i]
+      end
+      return counter
+    end
+    self.length
+  end
+
+  def my_map
+    mapped = []
+    for i in 0..self.length-1
+      mapped.push(yield self[i])
+    end
+    mapped
+  end
+
+  def my_inject(initial_operand)
+    operand = initial_operand
+    for i in 0..self.length-1
+      operand = yield operand, self[i]
+    end
+    operand
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +68,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for i in 0..self.length-1
+      yield self[i]
+    end
+    self
+  end
 end
